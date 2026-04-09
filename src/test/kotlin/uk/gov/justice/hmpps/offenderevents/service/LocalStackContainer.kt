0e0495c3fb6e77524fb16367675cad7bcae58b1b
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2026-04-09 16:06:15 +0100

    SDIT-3668: ✨ Random change to test gradle caching

--- src/test/kotlin/uk/gov/justice/hmpps/offenderevents/service/LocalStackContainer.kt
+++ src/test/kotlin/uk/gov/justice/hmpps/offenderevents/service/LocalStackContainer.kt
@@ -3,10 +3,9 @@ package uk.gov.justice.hmpps.offenderevents.service
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.test.context.DynamicPropertyRegistry
-import org.testcontainers.containers.localstack.LocalStackContainer
-import org.testcontainers.containers.localstack.LocalStackContainer.Service.SNS
 import org.testcontainers.containers.output.Slf4jLogConsumer
 import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.localstack.LocalStackContainer
 import org.testcontainers.utility.DockerImageName
 import java.io.IOException
 import java.net.ServerSocket
@@ -16,7 +15,7 @@ object LocalStackContainer {
   val instance by lazy { startLocalstackIfNotRunning() }
 
   fun setLocalStackProperties(localStackContainer: LocalStackContainer, registry: DynamicPropertyRegistry) {
-    registry.add("hmpps.sqs.localstackUrl") { localStackContainer.getEndpointOverride(SNS) }
+    registry.add("hmpps.sqs.localstackUrl") { localStackContainer.endpoint }
     registry.add("hmpps.sqs.region") { localStackContainer.region }
   }
 
@@ -30,7 +29,7 @@ object LocalStackContainer {
     return LocalStackContainer(
       DockerImageName.parse("localstack/localstack").withTag("4"),
     ).apply {
-      withServices(SNS, LocalStackContainer.Service.SQS)
+      withServices("sqs,sns")
       withEnv("HOSTNAME_EXTERNAL", "localhost")
       withEnv("DEFAULT_REGION", "eu-west-2")
       waitingFor(
@@ -44,7 +43,7 @@ object LocalStackContainer {
   private fun localstackIsRunning(): Boolean = try {
     val serverSocket = ServerSocket(4566)
     serverSocket.localPort == 0
-  } catch (e: IOException) {
+  } catch (_: IOException) {
     true
   }
 }
