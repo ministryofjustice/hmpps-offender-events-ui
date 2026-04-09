509b1e46dd34eb40f547fe5481b9cee1539b0098
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-04-09 16:39:24 +0100

    SDIT-3668: ✨ Fix compilation warnings to test build cache (#369)

--- src/main/kotlin/uk/gov/justice/hmpps/offenderevents/service/OffenderEventStartUp.kt
+++ src/main/kotlin/uk/gov/justice/hmpps/offenderevents/service/OffenderEventStartUp.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.hmpps.offenderevents.service
 
+import com.google.gson.Gson
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.context.event.ContextRefreshedEvent
@@ -11,19 +12,19 @@ import uk.gov.justice.hmpps.offenderevents.data.EventRepository
 
 @Component
 class OffenderEventStartUp(
-  val offenderEventStore: OffenderEventStore,
-  val eventRepository: EventRepository,
-  val gson: com.google.gson.Gson,
-  val redisTemplate: RedisTemplate<Event, String>,
+  private val offenderEventStore: OffenderEventStore,
+  private val eventRepository: EventRepository,
+  private val gson: Gson,
+  private val redisTemplate: RedisTemplate<Event, String>,
 ) {
   private companion object {
-    val log: Logger = LoggerFactory.getLogger(this::class.java)
+    private val log: Logger = LoggerFactory.getLogger(this::class.java)
   }
 
   @EventListener(ContextRefreshedEvent::class)
   fun readAllEvents() {
     redisTemplate.execute { connection ->
-      val keys = connection.keys("event:*[^m]".toByteArray())
+      val keys = connection.keyCommands().keys("event:*[^m]".toByteArray())
       log.info("Found {} items in redis using db size", keys?.size ?: 0)
       keys?.forEach {
         eventRepository.findById(String(it).substring("event:".length)).ifPresent { event ->
